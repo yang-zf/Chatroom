6da0441e72a5f281d4b3855941e2ed4253e8209d
yang-zf <yzfchifan@163.com>
2020-08-12 07:23:11 -0700

    第一次代码提交

---
+++ include/server/model/friendmodel.hpp
@@ -0,0 +1,16 @@
+#ifndef FRIENDMODEL_H
+#define FRIENDMODEL_H
+
+#include "user.hpp"
+#include<vector>
+//提供好友信息的操作接口方法
+class FriendModel{
+public:
+    //添加好友关系
+    void insert(int userid,int friendid);
+
+    //返回用户好友列表 
+    vector<User> query(int userid);
+};
+
+#endif
